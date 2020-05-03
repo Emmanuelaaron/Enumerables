81498ca85f01f9ea4ddc3d88b45cf5a80f553615
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-05-03 09:47:30 +0300

    This adds my_each_with_index to the module

--- main.rb
+++ main.rb
@@ -5,9 +5,18 @@ module Enumerable
       yield(i)
     end
   end
-  
+
+  def my_each_with_index
+    i = 0
+    my_arr = to_a
+    until i == self.length
+      yield(my_arr[i], i)
+      i += 1
+    end
+    self
+  end
 end
 
-[2, 3, 4, 5, 6].my_each {|n|
-  p n * 2
+[2, 3, 4, 5, 6].my_each_with_index {|value, index|
+  puts "value: #{value}, index: #{index}"
 }
