44d206293a39075240232b86ff07befbb479821e
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-05-07 22:37:39 +0300

    Solves rubocop errors

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-# rubocop: disable Style/For, Style/GuardClause
-# rubocop: disable Style/RedundantSelf, Style/RedundantReturn
+# rubocop: disable Style/For, Style/CaseEquality, Metrics/PerceivedComplexity, Metrics/MethodLength
+# rubocop: disable Style/RedundantSelf, Metrics/CyclomaticComplexity, Metrics/ModuleLength
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
@@ -32,7 +32,7 @@ module Enumerable
     arr
   end
 
-  def my_all?(par1=nil)
+  def my_all?(par1 = nil)
     if par1
       for i in 0...length do
         return false unless par1 === self[i]
@@ -40,7 +40,7 @@ module Enumerable
     elsif block_given?
       for i in self do
         return false unless yield(i)
-      end  
+      end
     else
       for i in self do
         return false if i.nil? || i === false
@@ -49,7 +49,7 @@ module Enumerable
     true
   end
 
-  def my_any?(par1=nil)
+  def my_any?(par1 = nil)
     if par1
       for i in 0...length do
         return true if par1 === self[i]
@@ -57,7 +57,7 @@ module Enumerable
     elsif block_given?
       for i in self do
         return true if yield(i)
-      end  
+      end
     else
       for i in self do
         return true if i
@@ -66,7 +66,7 @@ module Enumerable
     false
   end
 
-  def my_none?(par1=nil)
+  def my_none?(par1 = nil)
     if par1
       for i in 0...length do
         return false if par1 === self[i]
@@ -74,7 +74,7 @@ module Enumerable
     elsif block_given?
       for i in self do
         return false if yield(i)
-      end  
+      end
     else
       for i in self do
         return false if i
@@ -83,16 +83,14 @@ module Enumerable
     true
   end
 
-  def my_count(par1=nil)
+  def my_count(par1 = nil)
     count = 0
     if par1
       for i in self do
-        if par1 == i
-          count += 1
-        end
+        count += 1 if par1 == i
       end
     elsif block_given?
-      my_each {|x| count += 1 if yield(x)}
+      my_each { |x| count += 1 if yield(x) }
 
     else
       count = self.length
@@ -100,7 +98,7 @@ module Enumerable
     count
   end
 
-  def my_map(proc=nil)
+  def my_map(proc = nil)
     my_arr = []
     if block_given?
       for i in self do
@@ -114,7 +112,7 @@ module Enumerable
     my_arr
   end
 
-  def my_inject(par1=nil, par2=nil)
+  def my_inject(par1 = nil, par2 = nil)
     my_arr = self.to_a
     if block_given? && par1
       accumulator = par1
@@ -127,23 +125,20 @@ module Enumerable
       for i in 1...my_arr.length do
         accumulator = yield(accumulator, my_arr[i])
       end
-
     elsif par1 && !block_given? && !par2
       accumulator = my_arr[0]
       for i in 1...my_arr.length do
-        accumulator =  accumulator.send(par1, my_arr[i])
+        accumulator = accumulator.send(par1, my_arr[i])
       end
-
     elsif par1 && par2
       accumulator = par1
       for i in 0...my_arr.length do
-        accumulator =  accumulator.send(par2, my_arr[i])
+        accumulator = accumulator.send(par2, my_arr[i])
       end
     end
     accumulator
   end
 end
 
-# rubocop: enable Style/For, Style/GuardClause
-# rubocop: enable Style/RedundantSelf, Style/RedundantReturn
-# Sum some numbers
+# rubocop: enable Style/For, Style/CaseEquality, Metrics/PerceivedComplexity, Metrics/MethodLength
+# rubocop: enable Style/RedundantSelf, Metrics/CyclomaticComplexity, Metrics/ModuleLength
